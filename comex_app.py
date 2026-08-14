a61a965a2ce64735c95323c71bf331dd573f8e20
Gabriel Maia <gabriel.maia@gep.com>
2026-08-14 11:08:42 -0300

    Update comex_app.py

--- comex_app.py
+++ comex_app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 
 home_page = st.Page("app_home.py", title="Menu", icon="🧭", default=True)
-brasil_page = st.Page("pages/1_Comex_Brasil.py", title="Comex Brasil")
-eua_page = st.Page("pages/2_Comex_EUA.py", title="Comex EUA")
+brasil_page = st.Page(
+    "pages/1_Comex_Brasil.py", title="Comex Brasil", url_path="comex-brasil"
+)
+eua_page = st.Page(
+    "pages/2_Comex_EUA.py", title="Comex EUA", url_path="comex-eua"
+)
 
 pg = st.navigation([home_page, brasil_page, eua_page])
 pg.run()
